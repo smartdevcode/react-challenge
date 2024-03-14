9c133f932df1440ab7d2d3381a2c1849053010d9
Senior developer <senior.dev.software@gmail.com>
2024-03-14 12:35:31 -0700

    Updates

--- src/app/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import elementReducer from '../features/elementHighlighter/elementSlice';
-
-const rootReducer = combineReducers({
-  elementHighlighter: elementReducer,
-  // ... other feature reducers
-});
-
-export default rootReducer;
--- src/features/elementHighlighter/useElementHighlight.ts
+++
@@ -1,7 +0,0 @@
-import { useSelector } from 'react-redux';
-import { selectHighlightedNames } from './elementSlice';
-
-export const useElementHighlight = () => {
-  const names = useSelector(selectHighlightedNames);
-  return names;
-};
